c3a94bc7f6acf99fa65ba0e1ffff17a130266c51
company <luffyzhao@vip.126.com>
2016-05-12 18:02:57 +0800

    添加 Visitor 类

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -5,6 +5,13 @@ class Index
 {
     public function index()
     {
-
+        echo \app\common\tools\Visitor::getBrowser();
+        echo "<br />";
+        echo \app\common\tools\Visitor::getBrowserVer();
+        echo "<br />";
+        echo \app\common\tools\Visitor::getIP();
+        echo "<br />";
+        echo \app\common\tools\Visitor::getOs();
+        echo "<br />";
     }
 }
