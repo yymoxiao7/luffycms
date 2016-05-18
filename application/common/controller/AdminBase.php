dd87d8a216190065581999d1a9d3f113a7f9f811
company <luffyzhao@vip.126.com>
2016-05-18 09:58:10 +0800

    添加breadcrumb

--- application/common/controller/AdminBase.php
+++ application/common/controller/AdminBase.php
@@ -2,6 +2,7 @@
 namespace app\common\controller;
 
 use think\Controller;
+use think\Db;
 use think\Session;
 
 /**
@@ -13,6 +14,7 @@ class AdminBase extends Controller
     {
         parent::__construct();
 
+        $this->getBreadcrumb();
         // ajax请求返回json数据
         // Config::set('default_ajax_return', true);
 
@@ -29,5 +31,35 @@ class AdminBase extends Controller
         Session::clear();
 
         return $this->success('退出成功！', '/admin/common/login');
+    }
+
+    /**
+     * 获取当前位置
+     * @author luffy<luffyzhao@vip.126.com>
+     * @dateTime 2016-05-18T09:33:02+0800
+     * @return   [type]                          [description]
+     */
+    protected function getBreadcrumb()
+    {
+        $breadcrumb = [];
+        $rule       = CONTROLLER_NAME . '/' . ACTION_NAME;
+
+        $isHere = Db::table('rule')
+            ->field('parent_id,title,name')
+            ->where('name', $rule)->find();
+
+        if (empty($isHere)) {
+            return false;
+        }
+
+        if ($isHere['parent_id'] !== 0) {
+            $breadcrumb[] = Db::table('rule')
+                ->field('parent_id,title,name')
+                ->where('id', $isHere['parent_id'])->find();
+        }
+
+        $breadcrumb[] = $isHere;
+
+        $this->assign('breadcrumb', $breadcrumb);
     }
 }
