956347efd748b5c433082df9269632663c1ed96f
company <luffyzhao@vip.126.com>
2016-05-31 18:01:27 +0800

    友情链接列表

--- application/admin/controller/Links.php
+++ application/admin/controller/Links.php
@@ -8,5 +8,14 @@ use app\common\controller\AdminBase;
  */
 class Links extends AdminBase
 {
-
+    /**
+     * 友情链接列表
+     * @author luffy<luffyzhao@vip.126.com>
+     * @dateTime 2016-05-31T18:00:49+0800
+     * @return   [type]                   [description]
+     */
+    public function index()
+    {
+        # code...
+    }
 }
